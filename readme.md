cf77f7cf0858f6afe0ea62e90b8267fd09b8a58d
Carl.Edmunds <carledmunds90@googlemail.com>
2018-01-18 10:22:17 +0000

    Initial project commit
    - Adds readme file
    - Adds basic Spring Boot application setup

---
+++ readme.md
@@ -0,0 +1,3 @@
+# spring_reactjs_demo
+This is a small demo playing with Spring boot and ReactJS together in order to create a
+front-end and back-end together in the same project. 
---
+++ src/server/main/java/com/ceddy/spring_react/SpringReactApplication.java
@@ -0,0 +1,12 @@
+package com.ceddy.spring_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactApplication.class, args);
+	}
+}
